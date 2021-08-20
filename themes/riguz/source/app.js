70f342a7d645d20f57b328a298e4902ade53230c
Riguz Lee <solee.linux@gmail.com>
2021-08-20 21:29:00 +0800

    added toc

---
+++ themes/riguz/source/app.js
@@ -0,0 +1,10 @@
+hljs.initHighlightingOnLoad();
+
+tocbot.init({
+    tocSelector: '#blog-toc',
+    contentSelector: '.blog-content',
+    headingSelector: 'h1, h2, h3',
+    hasInnerContainers: true,
+    positionFixedSelector: '#blog-toc'
+  });
+  
