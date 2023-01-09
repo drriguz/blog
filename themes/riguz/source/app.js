9f1500b25a968ca703ae578e4d72a17b7e1b5143
Riguz Lee <lihaifeng2@wps.cn>
2023-01-09 15:29:40 +0800

    customize bulma css, fix FOUC

--- themes/riguz/source/app.js
+++ themes/riguz/source/app.js
@@ -1,33 +1,36 @@
-VANTA.BIRDS({
-  el: "#birds-canvas",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  backgroundColor: 0xffffff,
-  backgroundAlpha: 0,
-  quantity: 3.00
-});
+
 
 hljs.initHighlightingOnLoad();
 
-if($('#blog-toc').length) {
-  tocbot.init({
-      tocSelector: '#blog-toc',
-      contentSelector: '.blog-content',
-      headingSelector: 'h1, h2, h3',
-      hasInnerContainers: true,
-      positionFixedSelector: '#blog-toc'
-    });
-}
 
-$(document).ready(function() {
-  $(".navbar-burger").click(function() {
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-  });
+
+$(document).ready(function () {
+    if ($('#blog-toc').length) {
+        tocbot.init({
+            tocSelector: '#blog-toc',
+            contentSelector: '.blog-content',
+            headingSelector: 'h1, h2, h3',
+            hasInnerContainers: true,
+            positionFixedSelector: '#blog-toc'
+        });
+    }
+    $(".navbar-burger").click(function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+
+    VANTA.BIRDS({
+        el: "#birds-canvas",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0xffffff,
+        backgroundAlpha: 0,
+        quantity: 3.00
+    });
 });
 
